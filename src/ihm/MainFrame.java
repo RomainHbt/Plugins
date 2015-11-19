585c155715f0cfbbf962e3a8b7c4af618c1927c8
Bellamy Matthieu <bellamy@a10p2.fil.univ-lille1.fr>
2015-11-19 16:44:06 +0100

    prémice de la fenetre

---
+++ src/ihm/MainFrame.java
@@ -0,0 +1,59 @@
+package ihm;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+public class MainFrame extends JFrame{
+	
+	private static final long serialVersionUID = -5459108069903947815L;
+	private JTextArea textArea;
+	
+	public MainFrame(String title) {
+		super(title);
+		
+		textArea = new JTextArea("Tapez votre texte ici...");
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 200);
+		this.addMenu();
+		this.add(textArea);
+		this.setVisible(true);
+	}
+	
+	public void addMenu() {
+		JMenuBar menu = new JMenuBar();
+		JMenu files = new JMenu("Fichier");
+		JMenu tools = new JMenu("Outils");
+		JMenu help = new JMenu("Aide");
+		
+		JMenuItem open = new JMenuItem("Ouvrir");
+		//open.addActionListener(new OpenListener(this));
+		JMenuItem exit = new JMenuItem("Quitter");
+		
+		exit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+				
+			}
+		});
+		
+		files.add(open);
+		files.addSeparator();
+		files.add(exit);
+		
+		menu.add(files);
+		menu.add(tools);
+		menu.add(help);
+		
+		setJMenuBar(menu);
+	}
+
+}
