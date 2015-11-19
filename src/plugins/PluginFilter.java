492e41e185f89ecf8e7f4e2a31695cdce52b7f46
RomainHbt <romain.hembert@gmail.com>
2015-11-19 16:20:55 +0100

    Début du projet

---
+++ src/plugins/PluginFilter.java
@@ -0,0 +1,14 @@
+package plugins;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class PluginFilter implements FilenameFilter {
+
+	@Override
+	public boolean accept(File dir, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/plugins/PluginFinder.java
@@ -0,0 +1,18 @@
+package plugins;
+
+import java.io.File;
+import java.util.List;
+import java.util.Timer;
+
+public class PluginFinder {
+
+	private PluginFilter filter;
+	private Timer timer;
+	private File dir;
+	private List<File> files;
+
+	public PluginFinder(String dir) {
+		
+	}
+	
+}
