cf00ebc93a767e43eee3f1376708ecffd1dc5aa2
Bellamy Matthieu <bellamy@a12p19.fil.univ-lille1.fr>
2015-11-24 16:16:39 +0100

    mettage des menu de la frame en attribut

--- src/ihm/MainFrame.java
+++ src/ihm/MainFrame.java
@@ -1,6 +1,5 @@
 package ihm;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,41 +7,52 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame{
 	private static final long serialVersionUID = -5459108069903947815L;
+	// The area where you can write your text
 	private JTextArea textArea;
+	// Menu bar on the top of the window
+	private JMenuBar menu;
+	// The different sub-menu
+	private JMenu files;
+	private JMenu tools;
+	private JMenu help;
+	// The item of each sub-menu
+	private JMenuItem open;
+	private JMenuItem exit;
 	
 	public MainFrame(String title) {
 		super(title);
 		
 		textArea = new JTextArea(5,30);
 		
+		// Customization of the window
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 200);
+		// Setup the menu
 		this.addMenu();
 		this.add(textArea);
+		// Display the window
 		this.setVisible(true);
 	}
 	
 	public void addMenu() {
-		JMenuBar menu = new JMenuBar();
-		JMenu files = new JMenu("Fichier");
-		JMenu tools = new JMenu("Outils");
-		JMenu help = new JMenu("Aide");
+		menu = new JMenuBar();
+		files = new JMenu("Fichier");
+		tools = new JMenu("Outils");
+		help = new JMenu("Aide");
 		
-		JMenuItem open = new JMenuItem("Ouvrir");
+		open = new JMenuItem("Ouvrir");
 		//open.addActionListener(new OpenListener(this));
-		JMenuItem exit = new JMenuItem("Quitter");
+		exit = new JMenuItem("Quitter");
 		
 		exit.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
-				
 			}
 		});
 		
